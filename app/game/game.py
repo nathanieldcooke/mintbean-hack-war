1fb2543975fdd486a3087f8e7d29cbad07a1c103
nathanieldcooke <nathanield.cooke@gmail.com>
2021-07-28 09:47:28 -0700

    create Game class to init game, and creat get rout in game_routes.py to init game

--- app/game/game.py
+++ app/game/game.py
@@ -1,7 +1,25 @@
-from deck import Deck
+import random
+import string
+from app.game.deck import Deck
 
 
+class Game:
+    def __init__(self, players):
+        self.players = players
+        self.deck = Deck(players)
+        self.playerDecks = self.deck.playerDecks
+        self.deck = self.deck.deckDict
+        self.game_code = self.generateGameCode()
 
-# newDeck = Deck(2)
-# print(newDeck.deckList)
-# print(newDeck.playerDecks)
+    def generateGameCode(self):
+        letters = string.ascii_uppercase
+        code = ''.join(random.choice(letters) for _i in range(6))
+        return code
+
+
+# game = Game(2)
+
+# print(game.players)
+# print(game.playerDecks)
+# print(game.game_code)
+
