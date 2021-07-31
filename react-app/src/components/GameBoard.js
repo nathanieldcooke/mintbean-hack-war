57f1717bdd561be110d50f98d147f4140b5acc95
nathanieldcooke <nathanield.cooke@gmail.com>
2021-07-30 18:40:28 -0700

    add moveCardsToBattle and moveCardsToWinnerDeck

--- react-app/src/components/GameBoard.js
+++ react-app/src/components/GameBoard.js
@@ -39,21 +39,144 @@ const GameBoard = () => {
     let [eScore, setEScore] = useState(1000)
     let [score, setScore] = useState(1000)
     let [deck, setDeck] = useState([])
+    let [myStack, setMyStack] = useState('card-back')
+    let [eStack, setEStack] = useState('card-back')
+
 
     useEffect(() => {
         console.log(gameState)
-        if (gameState[`player${pNum}`] && !gameState[`player${pNum == 1 ? 2 : 1}`]) {
+        if (gameState[`player${pNum}`] && gameState[`player${pNum == 1 ? 2 : 1}`]) {
+            gameStepper()
+        } else if (gameState[`player${pNum}`]) {
             startGame()
-        } else {
             // handle game move
 
             // show recent play
             // showLastPlay()
+
             // setGame for next play 
-            setNextRound()
+            // setNextRound()
+
         }
 
     }, [gameState])
+
+    const sleep = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    const gameStepper = async () => {
+        // await sleep(2000)
+        await moveCardsToBattle()
+        await moveCardsToWinnerDeck()
+    }
+
+    const moveCardsToWinnerDeck = async () => {
+        let playerHand = gameState[`player${pNum}`].curr_play
+        let ePlayerHand = gameState[`player${pNum == 1 ? 2 : 1}`].curr_play
+
+        let winLose = gameState['winLose']
+        let countOf1 = winLose.filter(num => 1 === num).length
+        let countOf2 = winLose.filter(num => 2 === num).length
+
+        let winStack = null 
+
+        if (countOf1 > countOf2) { // player1 wins battle
+            console.log('hello')
+            if (pNum == countOf1)  {
+                console.log('hello1111')
+                winStack = setMyStack
+            } else {
+                console.log('hello2222')
+                winStack = setEStack
+            }
+            // winStack = setMyStack
+        } else { // player2 wins battle
+            console.log('heeeee', pNum, countOf2)
+            if (pNum == countOf2) {
+                console.log('heeOOO')
+                winStack = setMyStack
+            } else {
+                console.log('heeEEE')
+                winStack = setEStack
+            }
+            // winStack = setEStack
+        }
+
+
+        await sleep(2000)
+        // let card1 = aCard1
+        winStack(playerHand[0])
+        aSetCard1('play_b_1')
+
+
+        await sleep(500)
+        // let ecard1 = aECard1
+        winStack(ePlayerHand[0])
+        aESetECard1('play_b_1')
+
+        await sleep(500)
+        // let card1 = aCard1
+        winStack(playerHand[1])
+        aSetCard2('play_b_2')
+
+
+        await sleep(500)
+        // let ecard1 = aECard1
+        winStack(ePlayerHand[1])
+        aESetECard2('play_b_2')
+
+
+        await sleep(500)
+        // let card1 = aCard1
+        winStack(playerHand[2])
+        aSetCard3('play_b_3')
+
+
+        await sleep(500)
+        // let ecard1 = aECard1
+        winStack(ePlayerHand[2])
+        aESetECard3('play_b_3')
+
+        await sleep(500)
+        // let ecard1 = aECard1
+        winStack('card-back')
+        // aESetECard3('card-back')
+    }
+
+    const moveCardsToBattle = async () => {
+        let playerHand = gameState[`player${pNum}`].curr_play
+        let ePlayerHand = gameState[`player${pNum == 1 ? 2 : 1}`].curr_play
+
+        console.log(playerHand, ePlayerHand)
+
+        await sleep(2000)
+
+        setCard1('play_b')
+        setECard1('play_b')
+
+        aSetCard1(playerHand[0])
+        aESetECard1(ePlayerHand[0])
+
+        await sleep(500)
+
+        setCard2('play_b')
+        setECard2('play_b')
+
+        aSetCard2(playerHand[1])
+        aESetECard2(ePlayerHand[1])
+
+        await sleep(500)
+
+        setCard3('play_b')
+        setECard3('play_b')
+
+        aSetCard3(playerHand[2])
+        aESetECard3(ePlayerHand[2])
+
+        await sleep(500)
+
+    }
 
     const setNextRound = () => {
         let playerState = gameState[`player${pNum}`]
@@ -155,7 +278,7 @@ const GameBoard = () => {
                         <span>Deck: <span>{52 - deck?.length}</span>/52</span>
                     </div>
                 </div>
-                <Card imgName={"card-back"} />
+                <Card imgName={myStack} />
             </div>
 
             <div id='center-board'>
@@ -276,7 +399,7 @@ const GameBoard = () => {
                         disabled={!moves[0] || !moves[1] || !moves[2]} 
                     >Battle</button>
                 </div>
-                <Card imgName={"card-back"}/>
+                <Card imgName={eStack}/>
             </div>
         </div>
     );
