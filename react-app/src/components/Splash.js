a4d74ad1544227f6a00394b1a67a8f87c231a9d8
nathanieldcooke <nathanield.cooke@gmail.com>
2021-08-02 16:04:27 -0700

    add about dev slide to splash page

--- react-app/src/components/Splash.js
+++ react-app/src/components/Splash.js
@@ -4,12 +4,42 @@ import './Splash.css'
 const Splash = ({splashOpen, setSplashOpen, open, setOpen, activeGame}) => {
     const [splashSlide, setSplashSlide] = useState(true)
     const [rulesSlide, setRulesSlide] = useState(false)
-    const [backImg, setBackImg] = useState('splash_wb.jpg')
     const [aboutMeSlide, setAboutMeSlide] = useState(false)
+    const [backImg, setBackImg] = useState('splash_wb.jpg')
 
     const startGame = () => {
         setSplashOpen(true)
         setOpen(false)
+    }
+
+    const toRules = () => {
+        setSplashSlide(false)
+        setAboutMeSlide(false)
+
+        setRulesSlide(true)
+        setBackImg('game-rule.png')
+    }
+
+    const fromRules = () => {
+        setRulesSlide(false)
+
+        setSplashSlide(true)
+        setBackImg('splash_wb.jpg')
+    }
+
+    const toAboutMe = () => {
+        setSplashSlide(false)
+        setRulesSlide(false)
+
+        setAboutMeSlide(true)
+        setBackImg('me-comp-back.jpeg')
+    }
+
+    const fromAboutMe = () => {
+        setAboutMeSlide(false)
+        
+        setSplashSlide(true)
+        setBackImg('splash_wb.jpg')
     }
 
     const splashComp = (
@@ -19,11 +49,7 @@ const Splash = ({splashOpen, setSplashOpen, open, setOpen, activeGame}) => {
             <div><span id='line-span' >Where Luck Meets Reading Your Opponent</span></div>
             <div id={'div-splash-comp'}>
                 <button
-                    onClick={() => {
-                        setSplashSlide(false)
-                        setRulesSlide(true)
-                        setBackImg('game-rule.png')
-                    }}
+                    onClick={toRules}
                 >Rules</button>
                 {
                 activeGame
@@ -37,7 +63,9 @@ const Splash = ({splashOpen, setSplashOpen, open, setOpen, activeGame}) => {
                     onClick={startGame}
                 >Start Playing</button>
                 }   
-            <button>About Dev</button>
+                <button
+                    onClick={toAboutMe}
+                >About Dev</button>
             </div>
         </div>
     )
@@ -46,11 +74,7 @@ const Splash = ({splashOpen, setSplashOpen, open, setOpen, activeGame}) => {
         <div id={'rules-comp'}>
             <div id={'div-splash-comp'} className='rules'>
                     <button
-                        onClick={() => {
-                            setSplashSlide(true)
-                            setRulesSlide(false)
-                            setBackImg('splash_wb.jpg')
-                        }}
+                        onClick={fromRules}
                     >Back</button>
                 {
                     activeGame
@@ -64,14 +88,60 @@ const Splash = ({splashOpen, setSplashOpen, open, setOpen, activeGame}) => {
                             onClick={startGame}
                         >Start Playing</button>
                 }
-                <button>About Dev</button>
+                <button
+                    onClick={toAboutMe}
+                >About Dev</button>
             </div>
         </div>
     )
 
     const aboutMeComp = (
         <div id={'me-comp'}>
-            
+            {/* <div id='profile-img' style={{backgroundImage: `url('../../img/profile_img.jpg')`}}></div> */}
+            <div>
+
+                <div id='pro-img'><img src='../../img/profile_img.jpg' /></div>
+                <div id='about-me-text'>
+                    <span id='me-title'>Nathaniel Cooke</span>
+                    <span id='me-icons'>
+                        <span
+                            onClick={() => window.open(`https://github.com/nathanieldcooke`, '_blank', 'noopener noreferrer')}
+                        ><i className="fab fa-github"></i></span>
+                        <span
+                            onClick={() => window.open(`https://www.linkedin.com/in/nathaniel-cooke-257a1363/`, '_blank', 'noopener noreferrer')}
+                        ><i className="fab fa-linkedin"></i></span>
+                        <span
+                            onClick={() => window.open(`https://nathanieldcooke.github.io/`, '_blank', 'noopener noreferrer')}
+                        ><i className="fas fa-folder-open"></i></span>
+                    </span>
+                    <div id='about-me-sub-text'>
+                        <span id='me-text1'>I have strong Full-Stack Web-Development experience... having built a few websites and games. I continue to grow as a developer everyday.</span>
+                        <span id='me-text2'>While on this journey. I hope we can build some awesome applications together!</span>
+                    </div>
+                </div>
+                <div id='about-me-btns' >
+                    <div id={'div-splash-comp'} className='rules'>
+                        <button
+                            onClick={toRules}
+                        >Rules</button>
+                        {
+                            activeGame
+                                ?
+                                <button
+                                    id='in-game-close'
+                                    onClick={() => setSplashOpen(true)}
+                                >Close</button>
+                                :
+                                <button
+                                    onClick={startGame}
+                                >Start Playing</button>
+                        }
+                        <button
+                            onClick={fromAboutMe}
+                        >Back</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
     return (
@@ -81,6 +151,7 @@ const Splash = ({splashOpen, setSplashOpen, open, setOpen, activeGame}) => {
             <div style={{ backgroundImage: `url(../../img/${backImg})` }} id={'splash-div'}>
                 {splashSlide && splashComp}
                 {rulesSlide && rulesComp}
+                {aboutMeSlide && aboutMeComp}
             </div>
         </>
     )
